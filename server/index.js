301fce8dc89716b25f12ffeeb7c4bf7f5d521094
Kaushik Goswami <thekaushikgoswami@gmail.com>
2025-01-05 10:07:05 +0530

    day 1

--- server/index.js
+++ server/index.js
@@ -1,6 +1,14 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const db = require('./models');
 
-app.listen(3001, () => {
-    console.log("Server running on port 3001")
-})
+const app = express();
+
+db.sequelize.sync().then(() => {
+
+    app.listen(3001, () => {
+        console.log("Server running on port 3001")
+    });
+
+    }
+)
+

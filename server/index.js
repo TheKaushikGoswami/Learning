c26e05eee473f93db691515336c5e4f6a0908d05
Kaushik Goswami <thekaushikgoswami@gmail.com>
2025-01-05 10:31:23 +0530

    Backend done

--- server/index.js
+++ server/index.js
@@ -2,6 +2,12 @@ const express = require('express');
 const db = require('./models');
 
 const app = express();
+app.use(express.json());
+
+// Routers
+
+const postRouter = require('./routes/Posts');
+app.use('/posts', postRouter);
 
 db.sequelize.sync().then(() => {
 

09abe1eed204a5b554a8773bcb91e7dd93f3cff0
Kaushik Goswami <thekaushikgoswami@gmail.com>
2025-01-07 11:05:08 +0530

    Add Comments Frontend complete

--- client/src/pages/Post.js
+++ client/src/pages/Post.js
@@ -5,23 +5,56 @@ import axios from 'axios';
 function Post() {
     let { id } = useParams();
     const [post, setPost] = useState({});
+    const [comments, setComments] = useState([]);
+    const [comment, setComment] = useState('');
 
     useEffect(() => {
         axios.get('http://localhost:3001/posts/' + id).then((res) => {
             setPost(res.data);
         });
-    });
+
+        axios.get('http://localhost:3001/comments/' + id).then((res) => {
+            setComments(res.data);
+        });
+    }, []);
+
+    const addComment = () => {
+        axios.post('http://localhost:3001/comments', {
+            comment: comment,
+            PostId: id
+        }).then(() => {
+            const newComment = { comment: comment };
+            setComments([...comments, newComment]);
+            setComment('');
+        });
+    }
 
     return (
         <div>
             <div className='postPage'>
-                <div className='post'>
+                <div className='postitem'>
                     <div className='title'>{post.title}</div>
                     <div className='content'>{post.content}</div>
                     <div className='author'>- {post.username}</div>
                 </div>
                 <div className='comments'>
-                    Comment Section
+                    <div className='addComment'>
+                        <input type='text' placeholder='Add a comment' value={comment} onChange={(event) => {
+                            setComment(event.target.value);
+                        }} />
+                        <button onClick={addComment}>Submit</button>
+                    </div>
+                    <div className='commentList'>
+                        {
+                            comments.map((comment, key) => {
+                                return (
+                                    <div key={key} className='comment'>
+                                        <div className='comment-content'>{comment.comment}</div>
+                                    </div>
+                                )
+                            })
+                        }
+                    </div>
                 </div>
             </div>
         </div>

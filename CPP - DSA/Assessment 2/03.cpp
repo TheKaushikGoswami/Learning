dbe60dd347f2a52af511669aae0d5b73ac259280
Kaushik Goswami <thekaushikgoswami@gmail.com>
2025-03-09 18:20:12 +0530

    Minor Edits

--- CPP - DSA/Assessment 2/03.cpp
+++ CPP - DSA/Assessment 2/03.cpp
@@ -6,24 +6,24 @@ using namespace std;
 
 string convert(int num) {
     string result = "";
-    string ones[] = {"", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"};
-    string teens[] = {"Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"};
+    string below_20[] = {"", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"};
     string tens[] = {"", "", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"};
 
-    if (num < 10) {
-        result = ones[num];
-    } else if (num < 20) {
-        result = teens[num - 10];
+    if(num == 0) {
+        return "Zero";
+    }
+    else if (num < 20) {
+        result += below_20[num];
     } else if (num < 100) {
-        result = tens[num / 10] + " " + ones[num % 10];
+        result += tens[num / 10] + " " + below_20[num % 10];
     } else if (num < 1000) {
-        result = ones[num / 100] + " Hundred " + convert(num % 100);
-    } else if (num < 1000000) {
-        result = convert(num / 1000) + " Thousand " + convert(num % 1000);
-    } else if (num < 1000000000) {
-        result = convert(num / 1000000) + " Million " + convert(num % 1000000);
+        result += below_20[num / 100] + " Hundred " + convert(num % 100);
+    } else if (num < 100000) {
+        result += convert(num / 1000) + " Thousand " + convert(num % 1000);
+    } else if (num < 10000000) {
+        result += convert(num / 100000) + " Lakh " + convert(num % 100000);
     } else {
-        result = convert(num / 1000000000) + " Billion " + convert(num % 1000000000);
+        result += convert(num / 10000000) + " Crore " + convert(num % 10000000);
     }
 
     return result;
@@ -33,11 +33,7 @@ int main() {
     int num;
     cin >> num;
 
-    if (num == 0) {
-        cout << "Zero" << endl;
-    } else {
-        cout << convert(num) << endl;
-    }
+    cout << convert(num) << endl;
 
     return 0;
 }
